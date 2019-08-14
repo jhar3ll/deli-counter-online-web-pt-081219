4c12592f6900608e12241dcaddb8d15da257a1f4
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-14 16:40:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,50 @@
-def line(array)
-  if array.empty?
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@jhar3ll 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+ 
+0
+0 5 jhar3ll/deli-counter-online-web-pt-081219
+forked from learn-co-students/deli-counter-online-web-pt-081219
+ Code  Pull requests 0  Projects 0  Security  Insights  Settings
+deli-counter-online-web-pt-081219/deli_counter.rb
+ deniznida fix merge conflicts
+8f72297 on Sep 23, 2015
+28 lines (24 sloc)  553 Bytes
+    
+# Write your code here.
+
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
-    place_in_line = "The line is currently:"
-    array.each_with_index(1) do |customer, counter|
-    place_in_line <<" #{counter}. #{customer}"
-  end
-   puts place_in_line
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
 
-def take_a_number(array, name)
-  array << name 
-  puts "Welcome, #{name}. You are number #{array.length} in line."
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
-def now_serving(array)
-  if array.empty?
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array.first}."
-    array.shift
+    puts "Currently serving #{deli.first}."
+    deli.shift
   end
-  
 end
