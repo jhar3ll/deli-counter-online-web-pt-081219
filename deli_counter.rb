ef2b721313a07bd4a88bff613aa6154ec0b3d315
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-14 16:43:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 def line(array)
+  
   if array.empty?
     puts "The line is currently empty."
   else
@@ -6,16 +7,26 @@ def line(array)
     array.each.with_index(1) do |customer, counter|
     place_in_line <<" #{counter}. #{customer}"
   end
+  
    puts place_in_line
   end
+  
 end
 
+
+
+
+
 def take_a_number(array, name)
+  
   array << name 
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
+
+
 def now_serving(array)
+  
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
