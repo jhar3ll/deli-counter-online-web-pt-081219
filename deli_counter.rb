3849251515bdc9bf926b64c85773301fbdc7867c
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-14 16:25:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def line 
+  if
+    puts "The line is currently: #{i}. #{customer}"
+    elsif
+  puts "The line is currently empty"
+end
+
+
+
+def take_a_number(array, string)
+  katz_deli = []
+  
+  i = 1 
+  array.each do |customer|
+    puts "Welcome, #{customer}. You are number #{i} in line"
+  end
+  
+  
+
+def now_serving
+  
